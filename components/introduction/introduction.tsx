7fcc30d98bb754da584bde6176659e382bac72a3
Blanka Semanová <bsemanova@deloittece.com>
2022-01-07 11:26:28 +0100

    Introduction component

--- components/introduction/introduction.tsx
+++ components/introduction/introduction.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import ProfilImg from './img/profil.jpeg';
+import LinkedIn from './img/in.svg';
+import GitHub from './img/git.svg';
+import Gmail from './img/gmail.svg';
 
 interface IntroductionProps {
   // picture: StaticImageData;
@@ -23,6 +26,32 @@ export const Introduction: React.FC<IntroductionProps> = ({
       <h1 className="introduction__container__name">{name}</h1>
       <h2 className="introduction__container__position">{position}</h2>
       <p className="introduction__container__text">{text}</p>
+    </div>
+    <div className="introduction__ikons">
+      <a
+        className="introduction__ikons__ikon"
+        href="https://www.linkedin.com/in/blanka-semanová"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image src={LinkedIn} alt="LinkedIn" layout="fill" objectFit="contain"/>
+      </a>
+      <a
+        className="introduction__ikons__ikon"
+        href="https://github.com/WhiteLady1"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image src={GitHub} alt="GitHub" layout="fill" objectFit="contain"/>
+      </a>
+      <a
+        className="introduction__ikons__ikon"
+        href="mailto: blanka.seman@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image src={Gmail} alt="Gmail" layout="fill" objectFit="contain"/>
+      </a>
     </div>
   </div>
 );
